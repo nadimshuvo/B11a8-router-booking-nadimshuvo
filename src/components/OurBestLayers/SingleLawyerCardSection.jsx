9f58621c9cf991128d240fa02f0bf5b9305e6432
nadimshuvo <nadimshuvo1997@gmail.com>
2025-04-22 14:38:28 +0600

    book appoinment section done

--- src/components/OurBestLayers/SingleLawyerCardSection.jsx
+++ src/components/OurBestLayers/SingleLawyerCardSection.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import divider from "../../assets/divider-line.png"
+import iIcon from "../../assets/i-icon.svg"
 
 const SingleLawyerCardSection = () => {
   return (
@@ -12,7 +14,10 @@ const SingleLawyerCardSection = () => {
             Lawyer’s Profile Details
           </h2>
           <p className="font-medium text-[#141414]/70 text-[16px] leading-[28px] max-w-[1028px] mx-auto">
-          Our platform connects you with verified, experienced doctors across various specialties — all at your convenience. Whether it's a routine checkup or urgent consultation, book appointments in minutes and receive quality care you can trust.
+            Our platform connects you with verified, experienced doctors across
+            various specialties — all at your convenience. Whether it's a
+            routine checkup or urgent consultation, book appointments in minutes
+            and receive quality care you can trust.
           </p>
         </div>
 
@@ -32,35 +37,58 @@ const SingleLawyerCardSection = () => {
               </h3>
 
               <span className="flex gap-10 font-medium max-sm:text-[10px] text-[16px] text-[#141414] mb-4">
-                <p className="opacity-60">
-                  Criminal Expert
-                </p>
-                <p className="opacity-70">
-                  &reg; License No: BD 12451254
-                </p>
+                <p className="opacity-60">Criminal Expert</p>
+                <p className="opacity-70">&reg; License No: BD 12451254</p>
               </span>
 
               <div className="flex items-center gap-4 mb-4">
-                <p className="font-bold text-[16px] text-[#141414]/70">Availability</p>
+                <p className="font-bold text-[16px] text-[#141414]/70">
+                  Availability
+                </p>
                 <div className="flex gap-4">
                   <span className="px-2.5 py-[5px] text-sm text-[#FFA000] bg-[#FFA000]/10 border border-[#FFA000]/20 rounded-full ">
                     Sunday
                   </span>
                   <span className="px-2.5 py-[5px] text-sm text-[#FFA000] bg-[#FFA000]/10 border border-[#FFA000]/20 rounded-full ">
-                  Monday
+                    Monday
                   </span>
                   <span className="px-2.5 py-[5px] text-sm text-[#FFA000] bg-[#FFA000]/10 border border-[#FFA000]/20 rounded-full ">
-                  Thursday
+                    Thursday
                   </span>
                 </div>
               </div>
 
               <div className="flex items-center gap-4 text-[16px]">
                 <p className="font-bold text-[#141414]/70">Consultation Fee:</p>
-                <p className="manrope-extrabold font-extrabold text-[#0EA106]">Taka : 273</p>
+                <p className="manrope-extrabold font-extrabold text-[#0EA106]">
+                  Taka : 273
+                </p>
               </div>
             </div>
           </div>
+        </div>
+
+        {/* Book Appoinment */}
+        <div className="book-appoinment mt-8 border border-[#0F0F0F]/15 rounded-2xl p-8 text-left">
+          <h3 className="text-center font-bold text-2xl text-[#141414] mb-4">Book an Appointment</h3>
+          <img src={divider} alt="divider" />
+
+          <div className="mt-4 flex justify-between items-center border-b pb-4 border-[#141414]/10">
+            <p className="plus-jakarta-sans-bold text-[18px] text-[#141414]">Availability</p>
+            <p className="plus-jakarta-sans-medium text-sm text-[#09982F] px-3.5 py-[7px] bg-[#09982F]/10 rounded-full border border-[#09982F]/20">Lawyer Available Today</p>
+          </div>
+
+          <div className="mt-4 inline-flex items-center gap-4 bg-[#FFA000]/10 rounded-full py-2 px-4">
+            <span>
+              <img src={iIcon} alt="" />
+            </span>
+            <p>Due to high client volume, we are currently accepting appointments for today only. We appreciate your understanding and cooperation.</p>
+          </div>
+
+          <div className="button-book-now bg-[#0EA106] m-6 text-center rounded-full">
+            <button className="font-bold text-[20px] py-3.5 text-white">Book Appointment Now</button>
+          </div>
+
         </div>
       </div>
     </section>
